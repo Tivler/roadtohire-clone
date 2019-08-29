a1b75dab643a08d37ad3d6ffc07bcec64c5696db
tivler <thomasdmckyer@gmail.com>
2019-08-29 15:50:38 -0400

    added hero

--- src/components/feature/ImageHero/ImageHero.jsx
+++ src/components/feature/ImageHero/ImageHero.jsx
@@ -2,29 +2,29 @@
 import React from "react";
 
 //CSS
-import "./ImageHero.css";
+import './_ImageHero.scss';
 
 const ImageHero = (props) => {
   return (
-    <div className="imageQuoteUnder-wrapper">
-      <div className="imageQuoteUnder-imageContainer">
-        <div className="imageQuoteUnder-ImageDiv">
-          <img alt="cohort 10" className="imageQuoteUnder-Image" src={props.url} />
-          <div className="test">
-            <p className="imageQuoteUnder-overlayText">{props.title}</p>
+      <section className="hero">
+        <div className="hero__container">
+          <img alt="cohort 10" className="hero__image" src={props.url} />
+          <div className="hero__overlay">
+            <p className="hero__overlay__title">{props.title}</p>
           </div>
         </div>
-      </div>
-      {/* <div className="imageQuoteUnder-quoteDiv">
-        <p className="imageQuoteUnder-quoteText">
-            {props.quote}
-        </p>
-        <p className='imageQuoteUnder-quoteAuthor'>
-            {props.author}
-        </p>
-      </div> */}
-    </div>
+      </section>
   );
 };
 
 export default ImageHero;
+
+
+//  {/* <div className="imageQuoteUnder-quoteDiv">
+//         <p className="imageQuoteUnder-quoteText">
+//             {props.quote}
+//         </p>
+//         <p className='imageQuoteUnder-quoteAuthor'>
+//             {props.author}
+//         </p>
+//       </div> */}
