816021590b4611fc1eaee82d6e5d00af0e84ce6f
tivler <thomasdmckyer@gmail.com>
2019-08-29 13:47:25 -0400

    added new responsiveness to home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import '../App.css';
 
 import VidHero from '../components/feature/VidHero/VidHero';
+import DidYouKnow from '../components/feature/DidYouKnow/DidYouKnow';
 
 class Home extends React.Component {
     render () {
         return (
         <>
             <VidHero />
+            <DidYouKnow />
             <div className="filler"></div>
         </>
     )
