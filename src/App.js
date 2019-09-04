89ea48a30b4936daf9eb7f13919b1cd1b5b22280
tivler <thomasdmckyer@gmail.com>
2019-09-04 08:42:23 -0400

    removed pages and components

--- src/App.js
+++ src/App.js
@@ -3,13 +3,6 @@ import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import Home from './pages/Home';
-import OurProgram from './pages/OurProgram';
-import CoreValues from './pages/CoreValues';
-import OurStories from './pages/OurStories';
-import Apply from './pages/Apply';
-import JCSU from './pages/JCSU';
-import FAQ from './pages/FAQ';
-import Privacy from './pages/Privacy';
 
 import './App.css';
 import AppNav from './components/core/AppNav/AppNav';
@@ -23,13 +16,6 @@ const App = () => {
       <Router>
         <AppNav />
             <Route exact path="/" component={Home} />
-            <Route exact path="/your-career" component={OurProgram} />
-            <Route exact path="/ourstories" component={OurStories} />
-            <Route exact path="/corevalues" component={CoreValues} />
-            <Route exact path="/apply" component={Apply} />
-            <Route exact path="/JCSU" component={JCSU} />
-            <Route exact path="/FAQ" component={FAQ} />
-            <Route exact path="/privacy-policy" component={Privacy} />
         <AppFooter />
       </Router>
     </div>
