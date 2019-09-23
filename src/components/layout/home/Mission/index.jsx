cf0ee1fbb0f28db12a5c1df2935fbcaef13843a9
tivler <thomasdmckyer@gmail.com>
2019-09-23 09:14:21 -0400

    bug fix

--- src/components/layout/home/Mission/index.jsx
+++ src/components/layout/home/Mission/index.jsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import './_styles.scss';
 
-import { Link } from 'react-router-dom';
-
 const Mission = () => {
     return (
         <>
@@ -10,7 +8,7 @@ const Mission = () => {
                 <div className="mission--wrapper">
                     <h2 className="mission__title">Our Impact</h2>
                     <p className="mission__statement">In our Road to Hire Academies, we’re looking for talented, tech-hungry young adults who are looking to break into the world of technology without a college degree! Being in tech isn’t easy, but you’ll be given access to world class resources, and state of the art facilities to learn and polish your craft.</p>
-                    <Link className="mission--button" to="/">Apply Now</Link>
+                    <a className="mission--button" href="https://boards.greenhouse.io/road2hire/jobs/1830097?gh_src=ee07e9541" rel="noopener noreferrer" target="_blank">Apply Now</a>
                 </div>
             </section>
         </>
